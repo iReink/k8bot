42313abbe2e2cf3990840c971548465dd1b2a592
iReink <ireink@gmail.com>
2025-10-14 01:03:25 +0500

    Создание новых таблиц

--- new_tables.py
+++ new_tables.py
@@ -2,7 +2,7 @@ import sqlite3
 
 DB_NAME = "stats.db"
 
-def create_shop_tables():
+def create_shop_tables_and_insert_items():
     conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
 
@@ -16,7 +16,7 @@ def create_shop_tables():
     );
     """)
 
-    # Таблица логов покупок
+    # Таблица логов покупок (без поля amount)
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS shop_log (
         chat_id INTEGER NOT NULL,
@@ -24,14 +24,26 @@ def create_shop_tables():
         date TEXT NOT NULL,
         time TEXT NOT NULL,
         item_name TEXT NOT NULL,
-        amount INTEGER NOT NULL,
         PRIMARY KEY (chat_id, user_id, date, time, item_name)
     );
     """)
 
+    # Добавление товаров в магазин
+    shop_items = [
+        ("cappuccino", 2, "Here's your cappuccino. Enjoy!", "CAACAgIAAyEFAASjKavKAAILDGjtFSC5Vwt8gdrqVcQioyNYB0fBAAKLhwACSN9pS7dWBha3mj-PNgQ"),
+        ("regular coffee", 1, "Your regular coffee is ready", "CAACAgIAAyEFAASjKavKAAILCmjtFQ1XrJF9ogxyMtUFcgT7zA-vAAJqfwACq5JxSz3gXCEUzl2ONgQ"),
+        ("pastel de nata", 1, "Good choice! Here's your pastel de nata", "CAACAgIAAyEFAASjKavKAAILDmjtFTSR6C5gEZhkrwXT0XuXQBRBAAKVggACBBZpS4GBz0PyVczoNgQ"),
+        ("water with lemon", 0, "Your water with lemon, have a nice day!", None)
+    ]
+
+    cursor.executemany("""
+    INSERT OR IGNORE INTO shop_items (item_name, price, response_text, sticker_file_id)
+    VALUES (?, ?, ?, ?);
+    """, shop_items)
+
     conn.commit()
     conn.close()
-    print("Таблицы 'shop_items' и 'shop_log' успешно созданы или уже существуют.")
+    print("Таблицы 'shop_items' и 'shop_log' созданы, товары добавлены.")
 
 if __name__ == "__main__":
-    create_shop_tables()
+    create_shop_tables_and_insert_items()
